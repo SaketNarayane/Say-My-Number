35bdb9b6f3dfcd4e4473b152f900b9cb743849db
Saket Narayane <snarayane@student.nitw.ac.in>
2017-08-31 02:38:37 +0530

    Add project files

---
+++ Say my number/src/say/my/number/SayMyNumber.java
@@ -0,0 +1,19 @@
+/*
+ * Creator Saket Narayane
+ */
+package say.my.number;
+
+/**
+ *
+ * @author Saket
+ */
+public class SayMyNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        start s=new start();
+        s.setVisible(true);
+    }
+}
